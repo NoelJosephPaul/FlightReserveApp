8774f92aeea67b5b8e6b8dfc3c308175d63e7edb
Noel Joseph Paul <noeljpaul2002@gmail.com>
2023-02-06 20:47:30 +0530

    Feb 6 2023 Update2

---
+++ client/src/pages/RescheduleTicket.js
@@ -0,0 +1,12 @@
+
+function RescheduleTicket()
+{
+    return (
+        <body id ="reschedulebody">
+            <div id ="rescheduleouterdiv">
+                
+            </div>
+        </body>
+    )
+}
+export default RescheduleTicket;
